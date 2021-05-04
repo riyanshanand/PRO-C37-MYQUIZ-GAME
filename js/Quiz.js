9c4ceff538ca6fd0c2e524d323e5931ccd47cc50
Riyansh Anand <76739189+riyanshanand@users.noreply.github.com>
2021-05-04 18:28:20 +0530

    Add files via upload

--- js/Quiz.js
+++ js/Quiz.js
@@ -30,27 +30,34 @@ class Quiz {
   }
 
   play(){
-    //write code here to hide question elements
-    question.hide()
-
-    //write code to change the background color here
-background(50,89,47)
-
-    //write code to show a heading for showing the result of Quiz
+    question.hide(); 
+    background("Yellow"); 
     fill(0); 
     textSize(30); 
     text("Result of the Quiz",340, 50); 
-    text("----------------------------",320, 65);
-
-    //call getContestantInfo( ) here
-
-
-    //write condition to check if contestantInfor is not undefined
-
-    //write code to add a note here
-
-    //write code to highlight contest who answered correctly
+    text("----------------------------",320, 65); 
+    Contestant.getPlayerInfo();
+     if(allContestants !== undefined){
+        debugger;
+         var display_Answers = 230;
+          fill("Blue"); 
+          textSize(20); 
+          text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230); 
+          for(var plr in allContestants){
+             debugger;
+              var correctAns = "2"; 
+              if (correctAns === allContestants[plr].answer) {
+              fill("Green");
+            }
+               else{ fill("red");
+              } 
+              display_Answers+=30; 
+              textSize(20); 
+              text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers);
+             } 
+            } 
+          }
     
   }
 
-}
+
